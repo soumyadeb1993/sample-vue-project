39dd0f74056f0a820f08210fc868ae3388b09205
soumyadeb <soumyadeb@arjava.in>
2021-05-21 10:24:30 +0530

    communicating event

--- main.js
+++ main.js
@@ -8,33 +8,33 @@ Vue.component('product', {
     template:`
         <div class="product">
             <div class="product-image">
-                <img width="200" height="200" :src="image" :alt="altText"/>
+                <img :src="image" :alt="altText"/>
             </div>
 
             <div class="product-info">
                 <h1>{{title}}</h1>
                 <p>{{description}}</p>
+                <p>{{inventory}}</p>
                 <p v-if="inventory > 10">In Stock</p>
                 <p v-else-if="inventory <= 5 && inventory > 0">Hurry!!!! Almost Sold !</p>
                 <p v-else-if="inventory <= 10 && inventory > 0">Almost Sold !</p>
                 <p v-else>Out of Stock</p>
                 <p>Shipping : {{shipping}}</p>
-            </div>
+                <product_info></product_info>
 
-            <product_info></product_info>
-
-            <div  v-for="(variant, index) in variants" 
+                <div  v-for="(variant, index) in variants" 
                     :key="variant.variantId" 
                     class="color-box" 
                     :style="{'background-color':variant.colour}"
                     @mouseover="updateProduct(index)">
+                </div>
+                
+                <button v-on:click="addToCart" :disabled="add_disabled">Add to Cart</button>
+                
+                <button v-on:click="removeFromCart" :disabled="remove_disabled">Remove From Cart</button>
+
             </div>
             
-            <button v-on:click="addToCart" :disabled="add_disabled">Add to Cart</button>
-            <div class="cart">
-                <p>Cart {{cart}}</p>
-            </div>
-            <button v-on:click="removeFromCart" :disabled="remove_disabled">Remove From Cart</button>
         </div>`,
     data(){
         return {
@@ -44,7 +44,7 @@ Vue.component('product', {
             altText:'Socks',
             selectedVariant:0,
             add_disabled:false,
-            remove_disabled:true,
+            remove_disabled:false,
             variants:[
                 {
                     variantId:1,
@@ -60,29 +60,14 @@ Vue.component('product', {
                     variant_qty:20
                 }
             ],
-            cart:0
         }
     },
     methods:{
         addToCart(){
-            this.cart += 1;
-            this.inventory -= 1;
-            if(this.inventory == 0){
-                this.add_disabled = true;
-            }
-            if(this.cart > 0){
-                this.remove_disabled = false;
-            }
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
         },
         removeFromCart(){
-            this.cart -= 1;
-            this.inventory += 1;
-            if(this.inventory > 0){
-                this.add_disabled = false;
-            }
-            if(this.cart == 0){
-                this.remove_disabled = true;
-            }
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index){
             this.selectedVariant = index;
@@ -125,6 +110,35 @@ Vue.component('product_info', {
 var app = new Vue({
     el:'#app',
     data:{
-        premium:false
+        premium:false,
+        cart:[]
+    },
+    methods:{
+        updateCart(id){
+            this.cart.push(id);
+            /* this.inventory -= 1;
+            if(this.inventory == 0){
+                this.add_disabled = true;
+            }
+            if(this.cart > 0){
+                this.remove_disabled = false;
+            } */
+        },
+        removeFromCart(id){
+            for(var i = this.cart.length - 1; i >= 0; i--) {
+                if (this.cart[i] === id) {
+                    this.cart.splice(i, 1);
+                    break;
+                }
+            }
+            /* this.cart -= 1;
+            this.inventory += 1;
+            if(this.inventory > 0){
+                this.add_disabled = false;
+            }
+            if(this.cart == 0){
+                this.remove_disabled = true;
+            } */
+        }
     }
 });
